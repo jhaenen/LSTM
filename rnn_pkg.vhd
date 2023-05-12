d2056b91bd727da2141032037236eea238dbabec
jhaenen <jasper@haenenweb.nl>
2023-05-12 13:10:40 +0200

    tanh LUT implementation

--- rnn_pkg.vhd
+++ rnn_pkg.vhd
@@ -10,5 +10,5 @@ package rnn_pkg is
     subtype data_t is sfixed(3 downto -12);
     subtype acc_t is sfixed(7 downto -24);
     subtype sigmoid_t is ufixed(0 downto -15);
-    subtype tanh_t is sfixed(0 downto -15);
+    subtype tanh_t is sfixed(1 downto -14);
 end;
